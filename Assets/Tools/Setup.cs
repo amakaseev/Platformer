bf0b4b2365be6ab0130889980b25905dd8c62a62
amakaseev <aleksey.makaseev@gmail.com>
2024-03-12 17:54:28 +0200

    add project

---
+++ Assets/Tools/Setup.cs
@@ -0,0 +1,25 @@
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public static class Setup {
+
+  static class Folders {
+    public static void CreateDefault(string root, params string[] folders) {
+      var fullPath = Path.Combine(Application.dataPath, root);
+      foreach (var folder in folders) {
+        var path = Path.Combine(fullPath, folder);
+        if (!Directory.Exists(path)) {
+          Directory.CreateDirectory(path);
+        }
+      }
+    }
+  }
+
+  [MenuItem("Tools/Setup/Create Default Folders")]
+  public static void CreateDefaultFolders() {
+    Folders.CreateDefault("_Project", "Animation", "Art", "Materials", "Prtefabs", "ScriptableObjects", "Scripts", "Settings");
+    UnityEditor.AssetDatabase.Refresh();
+  }
+
+}
