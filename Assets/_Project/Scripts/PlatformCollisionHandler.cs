6c0b339d043e95e1f1c4d66dbbdbba53f64f4477
amakaseev <aleksey.makaseev@gmail.com>
2024-03-15 18:05:45 +0200

    add moving platform

---
+++ Assets/_Project/Scripts/PlatformCollisionHandler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Platformer {
+  public class PlatformCollisionHandler : MonoBehaviour {
+    Transform platform; // The platform, if any we are on top of
+
+    void OnCollisionEnter(Collision other) {
+      if (other.gameObject.CompareTag("MovingPlatform")) {
+        ContactPoint contact = other.GetContact(0);
+        if (contact.normal.y < 0.5f)
+          return;
+
+        platform = other.transform;
+        transform.SetParent(platform);
+      }
+    }
+
+    void OnCollisionExit(Collision other) {
+      if (other.gameObject.CompareTag("MovingPlatform")) {
+        transform.SetParent(null);
+        platform = null;
+      }
+    }
+  }
+}
