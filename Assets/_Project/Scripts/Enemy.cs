aa446558646e43d483914b1db4a98c4dfbafc284
amakaseev <aleksey.makaseev@gmail.com>
2024-03-26 15:03:06 +0200

    add Eemy class

---
+++ Assets/_Project/Scripts/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using KBCore.Refs;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Platformer {
+
+  [RequireComponent(typeof(NavMeshAgent))]
+  public class Enemy : Entity {
+    [SerializeField, Self] NavMeshAgent agent;
+    [SerializeField, Child] Animator animator;
+
+    StateMachine stateMachine;
+
+    private void OnValidate() => this.ValidateRefs();
+
+  }
+
+}
