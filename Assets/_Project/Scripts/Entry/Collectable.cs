7be883e5ca34dc8b94f4001b6267fd2606f35534
amakaseev <aleksey.makaseev@gmail.com>
2024-03-27 21:21:27 +0200

    add PlayerHUD with healths and scores

--- Assets/_Project/Scripts/Entry/Collectable.cs
+++ Assets/_Project/Scripts/Entry/Collectable.cs
@@ -1,4 +1,15 @@
-﻿
+﻿using UnityEngine;
+
 namespace Platformer {
-  public class Collectable: Entity { }
+  public class Collectable: Entity {
+    [SerializeField] int score = 10; // FIXME set using factory
+    [SerializeField] IntEventChanel scoreChanel;
+
+    private void OnTriggerEnter(Collider other) {
+      if (other.CompareTag("Player")) {
+        scoreChanel.Invoke(score);
+        Destroy(gameObject);
+      }
+    }
+  }
 }
---
+++ Assets/_Project/Scripts/UI/ScoreUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+namespace Platformer {
+  public class ScoreUI : MonoBehaviour {
+    [SerializeField] TextMeshProUGUI scoreText;
+
+    private void Start() {
+      UpdateScore();
+    }
+
+    public void UpdateScore() {
+      StartCoroutine(UpdateScoreNextFrame());
+    }
+
+    IEnumerator UpdateScoreNextFrame() {
+      yield return null;
+      scoreText.text = GameManager.Instance.Score.ToString();
+    }
+  }
+}
